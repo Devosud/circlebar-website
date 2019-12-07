fe65539c30e38408d03c5f47196341a0659c96c3
vareversat <valentin.reversat22@gmail.com>
2019-12-07 18:18:45 +0100

    fix link into the footer component

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -12,7 +12,7 @@ export class FooterComponent implements OnInit {
   public primitivoLogo = require('../../assets/primitivo.png');
   public facebookLink = 'https://facebook.com';
   public linkedinLink = 'https://linkedin.fr';
-  public primitivoLink = 'https://linkedin.fr';
+  public primitivoLink = 'https://primitivo.fr';
   public devOsudLink = 'https://me.dev-o-sud.fr';
 
   constructor(public i18nService: I18nService) { }
